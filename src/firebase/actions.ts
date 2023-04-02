0d8a4ad87b4438dc30dcd43577abe3baf5e7fbb0
GiordanoT <giordanotinella@gmail.com>
2023-04-02 12:57:14 +0200

    slice fix

--- src/firebase/actions.ts
+++ src/firebase/actions.ts
@@ -3,15 +3,15 @@ import {collection, deleteDoc, doc, onSnapshot, setDoc, updateDoc} from '@fireba
 import {db} from '@/firebase/index';
 import {ActionObj, ActionValue} from "@/utils/type";
 import {ReduxAction} from "@/redux/actions";
-import {lobbySlice} from "@/redux/store/lobby";
+import {Slice} from "@reduxjs/toolkit";
 
 export class FirebaseAction {
-    static async load(collectionName: string): Promise<void> {
+    static async load(collectionName: string, slice: Slice): Promise<void> {
         const DOC = collection(db, collectionName);
         onSnapshot(DOC, (result) => {
             const objects: ActionObj[] = [];
             for(let doc of result.docs) { objects.push({...doc.data()} as ActionObj); }
-            ReduxAction.set(objects, lobbySlice);
+            ReduxAction.set(objects, slice);
         });
     }
 
--- src/pages/lobbies.tsx
+++ src/pages/lobbies.tsx
@@ -3,10 +3,11 @@ import Head from 'next/head';
 import Lobby from "@/components/lobbies/lobby";
 import {useEffect} from "react";
 import {FirebaseAction} from "@/firebase/actions";
+import {lobbySlice} from "@/redux/store/lobby";
 
 export default function Lobbies() {
     useEffect(() => {
-        FirebaseAction.load('lobbies').then();
+        FirebaseAction.load('lobbies', lobbySlice).then();
     });
     return (<>
         <Head>
